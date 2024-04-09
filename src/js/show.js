38516d764bcf07b64bb7d769aa595e3986ddb121
Luan Himmlisch <luan@himmlisch.com.mx>
2024-04-09 23:50:52 +0000

    Added times

--- src/js/show.js
+++ src/js/show.js
@@ -1,13 +1,23 @@
 (() => {
-    const show = (objects = [])  => {
+    const show = (objects = []) => {
         for (let i = 0; i < objects.length; i++) {
             const object = objects[i];
-            const windowHeight = window.innerHeight;
-            const elementTop = object.getBoundingClientRect().top;
-            const elementVisible = 150;
 
-            if (elementTop < windowHeight - elementVisible / 2) {
-                object.classList.add("active");
+            if (!object.classList.contains('active')) {
+                const windowHeight = window.innerHeight;
+                const elementTop = object.getBoundingClientRect().top;
+                const elementVisible = 150;
+
+                if (elementTop < windowHeight - elementVisible / 2) {
+                    if (object.classList.contains('wait')) {
+                        const queue = Object.values(objects).filter((v) => v.classList.contains('wait') && !v.classList.contains('active'));
+                        if (queue[0] === object) {
+                            setTimeout(() => object.classList.add("active"), 100);
+                        }
+                    } else {
+                        object.classList.add("active");
+                    }
+                }
             }
         }
     };
@@ -20,13 +30,13 @@
             object.classList.add(object.dataset['show-with']);
         }
     }
-    
+
     let ticking = false;
     window.addEventListener("scroll", () => {
         if (!ticking) {
             window.requestAnimationFrame(() => {
                 show(shows);
-               ticking = false;
+                ticking = false;
             });
             ticking = true;
         }
